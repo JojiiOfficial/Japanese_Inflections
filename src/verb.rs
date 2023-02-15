0bf0406313b58ee81b80f7197ce4787d7c9ce045
gorbit99 <gorbitgames@gmail.com>
2023-02-15 01:05:35 +0100

    Added docblocks for desirative

--- src/verb.rs
+++ src/verb.rs
@@ -997,12 +997,14 @@ impl Verb {
         ])
     }
 
+    /// Returns the desiderative form of the verb
     pub fn desiderative(&self) -> JapaneseResult<Word> {
         let mut stem = self.stem_long()?;
         stem.push_str("たい");
         Ok(stem)
     }
 
+    /// Returns the negative desiderative form of the verb
     pub fn negative_desiderative(&self) -> JapaneseResult<Word> {
         let mut stem = self.stem_long()?;
         stem.push_str("たくない");
